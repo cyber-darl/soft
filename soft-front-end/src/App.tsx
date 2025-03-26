a0d3541381e0f97953252fe9694412d5e0e46876
cyber-darl <onosdarlington939@gmail.com>
2025-03-26 13:47:53 +0100

    5.4 complete

--- soft-front-end/src/App.tsx
+++ soft-front-end/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
+import { useTonConnect } from "./hooks/useTonConnect";
 
 function App() {
   const {
@@ -9,7 +10,12 @@ function App() {
     recent_sender,
     owner_address,
     contract_balance,
+    sendIncrement,
+    sendDeposit,
+    sendWithdrawalRequest
   } = useMainContract();
+
+  const {connected} = useTonConnect();
   return (
     <div>
       <div>
@@ -27,6 +33,25 @@ function App() {
           <b>Counter Value</b>
           <div>{counter_value ?? "Loading..."}</div>
         </div>
+
+        {connected && (
+          <a onClick = {() => {sendIncrement();}}>
+Increment by 5
+          </a>
+        )}
+
+        <br/>
+        {connected && (
+          <a onClick = {() => {sendDeposit();}}>
+Request Deposit of 1 TON
+          </a>
+        )}
+        <br/>
+        {connected && (
+          <a onClick = {() => {sendWithdrawalRequest();}}>
+Request Withdrawal of 1 TON
+          </a>
+        )}
       </div>
     </div>
   );
--- soft-front-end/src/hooks/useMainContract.ts
+++ soft-front-end/src/hooks/useMainContract.ts
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react";
 import { MainContract } from "../contracts/Soft";
 import { useTonClient } from "./useTonClient";
 import { useAsyncInitialize } from "./useAsyncInitialize";
-import { Address, OpenedContract } from "ton-core";
+import { Address, OpenedContract, toNano } from "ton-core";
+import { useTonConnect } from "./useTonConnect";
 
 export function useMainContract() {
   const client = useTonClient();
+  const { sender } = useTonConnect();
+
+  const sleep = (time:number) => {
+    new Promise ((resolve)=> setTimeout (resolve, time));
+  }
   const [contractData, setContractData] = useState<null | {
     counter_value: number;
     recent_sender: Address;
@@ -34,6 +40,8 @@ export function useMainContract() {
         owner_address: val.owner_address,
       });
       setBalance(balance);
+      await sleep (5000); // sleep 5 seconds and poll value again
+      getValue();
     }
     getValue();
   }, [mainContract]);
@@ -42,5 +50,14 @@ export function useMainContract() {
     contract_address: mainContract?.address.toString(),
     contract_balance: balance,
     ...contractData,
+    sendIncrement: async () => {
+      return mainContract?.sendIncrement(sender, toNano("0.05"), 5)
+    },
+    sendDeposit: async () => {
+      return mainContract?.sendDeposit(sender, toNano("0.05"))
+    },
+    sendWithdrawalRequest: async () => {
+      return mainContract?.sendWithdrawalRequest(sender, toNano("0.05"), toNano("0.7"))
+    }
   };
 }
