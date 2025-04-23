dcfb2239c55e0333ce54673fa5bf7a4829a1257b
cyber-darl <onosdarlington939@gmail.com>
2025-04-24 00:10:21 +0100

    bot working nicely

--- soft_tg_bot/src/index.ts
+++ soft_tg_bot/src/index.ts
@@ -1,5 +1,8 @@
 import dotenv from "dotenv"
 import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import { beginCell, toNano } from "ton-core";
+import qs from "qs";
 
 dotenv.config();
 const bot = new Telegraf(process.env.TG_BOT_TOKEN!);
@@ -15,19 +18,86 @@ bot.start((ctx) =>
       },
     })
   );
+  bot.on(message("web_app_data"), (ctx) => ctx.reply("ok"));
 
   bot.hears("Increment by 5", (ctx) => {
-    // TODO: send increment transaction
-    ctx.reply("Incremented by 5");
+    const msg_body = beginCell() 
+    .storeUint(1, 32)
+    .storeUint(5, 32)
+    .endCell();
+
+    let link = `https://test.tonhub.com/transfer/${process.env.SC_ADDRESS}?${qs.stringify(
+      {
+        text: "Simple test transaction",
+        amount: toNano("0.05").toString(10),
+        bin: msg_body.toBoc({ idx: false }). toString("base64"),
+      }
+  )}`;
+
+  ctx.reply("To increment counter by 5, please sign a transaction:", {
+    reply_markup: {
+        inline_keyboard: [
+            [{
+                text: "Sign transaction",
+                url: link,
+            }]
+        ]
+    }
+});
   });
+
+
   bot.hears("Deposit 1 TON", (ctx) => {
+    const msg_body = beginCell().storeUint(2, 32).endCell();
+
+      
+    let link = `https://app.tonkeeper.com/transfer/${
+        process.env.SC_ADDRESS
+    }?${qs.stringify(
+        {
+            text: "Deposit 1 TON",
+            amount: toNano("1").toString(10),
+            bin: msg_body.toBoc({ idx: false}).toString("base64"),
+        }
+    )}`;
     // TODO: send deposit transaction
-    ctx.reply("Deposited 1 TON");
+    ctx.reply("To deposit 1 TON please sign a transaction:", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Sign transaction",
+              url: link,
+            }
+          ]
+        ]
+      }
+    });
+    
   });
   
   bot.hears ("Withdraw 0.7 TON", (ctx) => {
     // TODO: send withdraw transaction
-    ctx.reply("Withdrawn 0.7 TON");
+    const msg_body = beginCell().storeUint(3, 32).storeCoins(toNano(`0.7`)).endCell();
+  
+    let link = `https://test.tonhub.com/transfer/${
+      process.env.SC_ADDRESS
+    }?${qs.stringify({
+      text: "Withdraw 0.7 TON",
+      amount: toNano('0.05').toString (10),
+      bin: msg_body.toBoc({ idx: false }).toString("base64"), 
+    })}`;
+        
+    ctx.reply("To withdraw 0.7 TON please sign a transaction:", {
+      reply_markup: {
+          inline_keyboard: [
+              [{
+                  text: "Sign transaction",
+                  url: link,
+              }]
+          ]
+      }
+  });    
   });
 
   bot.launch();
